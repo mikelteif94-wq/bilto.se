4092a6de292bf203a6a8f019dbe7837aad9eb73a
mikelteif94-wq <mike.lteif94@gmail.com>
2026-05-27 18:06:10 +0200

    Updated MobileMenu.tsx

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,7 +5,6 @@ export type MobileMenuItem =
   | 'Sälj bil'
   | 'Förmedling'
   | 'Hitta bil'
-  | 'Köp bil'
   | 'Om oss'
   | 'Så funkar det';
 
@@ -18,7 +17,6 @@ interface MobileMenuProps {
 
 const ITEMS: MobileMenuItem[] = [
   'Hitta bil',
-  'Köp bil',
   'Sälj bil',
   'Om oss',
 ];
@@ -90,12 +88,6 @@ export default function MobileMenu({ open, onClose, active, onSelect }: MobileMe
                   }
                   if (item === 'Hitta bil') {
                     window.history.pushState({}, '', '/kop-bil');
-                    window.dispatchEvent(new PopStateEvent('popstate'));
-                    onClose();
-                    return;
-                  }
-                  if (item === 'Köp bil') {
-                    window.history.pushState({}, '', '/kop-bil-hjalp');
                     window.dispatchEvent(new PopStateEvent('popstate'));
                     onClose();
                     return;
