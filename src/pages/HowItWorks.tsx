c2380b4414262f072a05eeb958584330193051be
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-18 19:18:05 +0200

    Update 'Find a car' button link

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -414,7 +414,7 @@ export default function HowItWorks({ onBackHome, onSell, showSeo = false, pageTi
                     <button
                       type="button"
                       onClick={() => {
-                        window.history.pushState({}, '', '/kop-bil?quiz=start');
+                        window.history.pushState({}, '', '/kop-bil');
                         window.dispatchEvent(new PopStateEvent('popstate'));
                       }}
                       className="px-4 py-1.5 rounded-xl border-2 border-slate-800 text-slate-800 text-[13px] font-bold hover:bg-slate-800 hover:text-white active:scale-[0.98] transition-all whitespace-nowrap"
@@ -576,7 +576,7 @@ export default function HowItWorks({ onBackHome, onSell, showSeo = false, pageTi
                     <button
                       type="button"
                       onClick={() => {
-                        window.history.pushState({}, '', '/kop-bil?quiz=start');
+                        window.history.pushState({}, '', '/kop-bil');
                         window.dispatchEvent(new PopStateEvent('popstate'));
                       }}
                       className="px-4 py-1.5 rounded-xl border-2 border-slate-800 text-slate-800 text-[13px] font-bold hover:bg-slate-800 hover:text-white active:scale-[0.98] transition-all whitespace-nowrap"
