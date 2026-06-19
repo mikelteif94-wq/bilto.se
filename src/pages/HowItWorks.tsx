82b55959a0f3399af1d32004c4bedb8690dcc806
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-19 08:35:15 +0200

    Adjust image display on mobile

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1047,8 +1047,8 @@ export default function HowItWorks({ onBackHome, onSell, showSeo = false, pageTi
                 </a>
               </div>
 
-              <div className="relative max-w-[260px] sm:max-w-none mx-auto">
-                <div className="relative aspect-[5/4] rounded-[28px] sm:rounded-[36px] overflow-hidden">
+              <div className="relative">
+                <div className="relative aspect-[5/4] sm:aspect-[5/4] rounded-[28px] sm:rounded-[36px] overflow-hidden max-h-[220px] sm:max-h-none">
                   <img
                     src="/858c5bbb-bilto-hoodie.png"
                     alt="Bilexpert"
