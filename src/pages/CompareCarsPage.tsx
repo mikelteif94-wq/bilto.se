01538ee8bf3728e6e6108281f52864e8c9972dea
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-20 02:20:46 +0200

    Updated CompareCarsPage.tsx

--- src/pages/CompareCarsPage.tsx
+++ src/pages/CompareCarsPage.tsx
@@ -1000,7 +1000,7 @@ export default function CompareCarsPage({ onBackHome, pageSlug = 'kop-bil', hero
       {/* Hero */}
       <section className="pt-28 sm:pt-36 pb-12 sm:pb-16 px-5 sm:px-6 bg-[#0e6efe] relative overflow-hidden">
         <img
-          src="/ChatGPT_Image_24_maj_2026_22_13_42.png"
+          src="/ChatGPT_Image_8_maj_2026_09_33_53 copy.png"
           alt=""
           aria-hidden="true"
           className="absolute inset-0 w-full h-full object-cover object-center pointer-events-none select-none"
