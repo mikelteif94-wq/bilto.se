ca75a8bbc0ef48b79a2ad227df7d0a803d00ded8
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-21 13:54:14 +0200

    Update CTA text to 'Få bilköpshjälp'

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -640,7 +640,7 @@ export default function HowItWorks({ onBackHome, onSell, showSeo = false, pageTi
                   }}
                   className="h-11 px-6 rounded-xl bg-[#0e6efe] hover:bg-[#0a57cc] text-white font-semibold text-[14px] inline-flex items-center gap-2 transition-all"
                 >
-                  Läs mer om hur det fungerar
+                  Få bilköpshjälp
                   <ArrowRight className="w-4 h-4" />
                 </button>
               </div>
