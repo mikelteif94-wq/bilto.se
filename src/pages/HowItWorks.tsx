331d4b5f7af216aa289408065ab342168570ae92
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-22 17:24:39 +0200

    Remove p-6 padding from image

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -840,7 +840,7 @@ export default function HowItWorks({ onBackHome, onSell, showSeo = false, pageTi
               <img
                 src="/ee2543a0-987e-446d-9c5e-edb20859e84d.png"
                 alt="Karta över Sverige med upphämtningsorter"
-                className="w-full h-full object-contain block p-6"
+                className="w-full h-full object-contain block"
                 loading="lazy"
                 decoding="async"
               />
