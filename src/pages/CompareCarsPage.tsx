5215ed999ce2584bdcce74201fb673dc9e17b019
mikelteif94-wq <mike.lteif94@gmail.com>
2026-06-24 21:29:51 +0200

    Update CompareCarsPage hero section height

--- src/pages/CompareCarsPage.tsx
+++ src/pages/CompareCarsPage.tsx
@@ -1066,7 +1066,7 @@ export default function CompareCarsPage({ onBackHome, pageSlug = 'kop-bil', hero
       />
 
       {/* Hero */}
-      <section className="pt-28 sm:pt-36 pb-0 px-5 sm:px-6 relative overflow-hidden">
+      <section className="pt-28 sm:pt-36 pb-0 px-5 sm:px-6 relative overflow-hidden min-h-[100svh] flex flex-col justify-start">
         <img
           src="/hero/image.png"
           alt=""
