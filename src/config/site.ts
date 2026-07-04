eb9fb6668501b35aeb6a8f9c6f7937296d6c8a54
mikelteif94-wq <mike.lteif94@gmail.com>
2026-07-04 10:43:11 +0200

    Updated site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const PHONE = '08-5555 0200';
 export const PHONE_TEL = 'tel:+46855550200';
 export const EXPERT_NAME = 'Din Bilto-expert';
-export const EXPERT_PHOTO = 'https://images.pexels.com/photos/3760263/pexels-photo-3760263.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1';
+export const EXPERT_PHOTO = '/Man_in_car_showroom_portrait copy copy.png';
 export const FOUNDER_NAME = 'Din Bilto-expert';
-export const FOUNDER_PHOTO = 'https://images.pexels.com/photos/3760263/pexels-photo-3760263.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1';
+export const FOUNDER_PHOTO = '/Man_in_car_showroom_portrait copy copy.png';
